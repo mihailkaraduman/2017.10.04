e310572dd9a0f7432101b3a9b965e6804707fa1e
ST6 <st6@ST6-iMac.local>
2017-10-04 10:24:19 +0300

    git

--- 2017.10.04/ViewController.swift
+++ 2017.10.04/ViewController.swift
@@ -9,9 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var second = 0
+    var timer = Timer()
 
+    @IBOutlet weak var countDownLbl: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var stepperOutlet: UIStepper!
+    @IBOutlet weak var actionViewOutlet: UIView!
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        countDownLbl.text! = String(Int(sender.value))
+        print(sender.value)
+
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("ekrana dokundun")
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.timerFunc)), userInfo: nil, repeats: true)
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("ekrandan elini çektin")
+    }
+    
+    func timerFunc (){
+    print("çalışıyor")
+        if second <= 0 {
+            timer.invalidate()
+            second = 0
+            return
+            
+        }
+        second -= 1
+        print(second)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+                progressBar.progress = 0.0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
